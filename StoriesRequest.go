909a5774667552d774c7cf8e24079da5ee933b0e
Alexander Savinykh <asavinykh@nbrx.de>
2022-02-19 21:04:45 +0100

    Change type of LogEntries in StoriesRequest to array of pointers

--- StoriesRequest.go
+++ StoriesRequest.go
@@ -1,6 +1,6 @@
 package git_stories_api
 
 type StoriesRequest struct {
-	LogEntries []RepositoryLogEntry `json:"logEntries"`
-	TimeZone   string               `json:"timeZone"`
+	LogEntries []*RepositoryLogEntry `json:"logEntries"`
+	TimeZone   string                `json:"timeZone"`
 }
